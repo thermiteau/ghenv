1a428dd663a54c7954f559045f7a824316a9b186
David <david.cheal@thermite.au>
2025-07-07 16:19:22 +1000

    incremented version

--- src/ghenv/__init__.py
+++ src/ghenv/__init__.py
@@ -4,5 +4,5 @@ GitHub Environment Variable Management Package
 This package provides utilities for managing GitHub environment variables and secrets.
 """
 
-__version__ = "1.0.0"
+__version__ = "0.0.2"
 __author__ = "Thermite"
